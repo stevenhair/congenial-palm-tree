002246b4025793d1300bb817c00adf43bf949cce
Steven Hair <shair@hedgeserv.com>
2020-08-21 14:52:22 -0400

    add bug reproduction

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Client as BarClient } from './bar.client';
+import { Client as FooClient } from './foo.client';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    BarClient,
+    FooClient,
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+import { Client as BarClient } from './bar.client';
+import { Client as FooClient } from './foo.client';
+
 @Injectable()
 export class AppService {
+  constructor(private readonly foo: FooClient, private readonly bar: BarClient) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return `${this.foo.getMessage()} ${this.bar.getMessage()}`;
   }
 }
